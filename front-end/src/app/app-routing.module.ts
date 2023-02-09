672a0901b8ab543f7abeb08c747559963c3d6a2e
zmalek <zmalek@cosem.fr>
2023-02-09 15:23:45 +0100

    Remove ignored files

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
+import { AuthenticationPageComponent } from './authentication/authentication-page/authentication-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'authentication', component: AuthenticationPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationPageComponent } from './authentication/authentication-page/authentication-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationPageComponent
   ],
   imports: [
     BrowserModule,
